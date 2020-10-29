54c12255cf333714901e9ce8ae830a69e2f552fb
Michal Romanowski <michal.rom089@gmial.com>
2020-10-29 14:36:07 +0100

    add 3 xunit projects

---
+++ Michal.Romanowski.Service1.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Michal.Romanowski.Service1.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
